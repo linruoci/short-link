63d086ed4e9c58bdbab8d94e1c2c8a537a3837de
lin--jintao <1013240360@qq.com>
2024-06-12 12:59:16 +0800

    feature: 开发短链接后管系统用户注册功能

---
+++ shortlink/admin/src/main/java/com/ruoci/shortlink/admin/config/MyMetaObjectHandler.java
@@ -0,0 +1,26 @@
+package com.ruoci.shortlink.admin.config;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Slf4j
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.strictInsertFill(metaObject, "createTime", Date::new, Date.class);
+        this.strictInsertFill(metaObject, "updateTime", Date::new,  Date.class);
+        this.strictInsertFill(metaObject, "delFlag", () -> 0, Integer.class);
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.strictUpdateFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now());
+    }
+}
