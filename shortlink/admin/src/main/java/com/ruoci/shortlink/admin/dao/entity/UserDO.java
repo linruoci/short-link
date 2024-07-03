0a0f1de60c2d67f7dae14f1c5ca1d2a5a780b319
lin--jintao <1013240360@qq.com>
2024-07-03 22:13:43 +0800

    feature: 用户持久层对象修改

--- shortlink/admin/src/main/java/com/ruoci/shortlink/admin/dao/entity/UserDO.java
+++ shortlink/admin/src/main/java/com/ruoci/shortlink/admin/dao/entity/UserDO.java
@@ -1,18 +1,15 @@
 package com.ruoci.shortlink.admin.dao.entity;
 
-import com.baomidou.mybatisplus.annotation.FieldFill;
-import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.ruoci.shortlink.admin.common.database.BaseDO;
 import lombok.Data;
-
-import java.util.Date;
 
 /**
  * @Author: ruoci
  **/
 @Data
 @TableName("t_user")
-public class UserDO {
+public class UserDO extends BaseDO {
 
 
     /**
@@ -50,22 +47,6 @@ public class UserDO {
      */
     private Long deletionTime;
 
-    /**
-     * 创建时间
-     */
-    @TableField(fill = FieldFill.INSERT)
-    private Date createTime;
 
-    /**
-     * 修改时间
-     */
-    @TableField(fill = FieldFill.INSERT_UPDATE)
-    private Date updateTime;
-
-    /**
-     * 删除标识 0:未删除 1:已删除
-     */
-    @TableField(fill = FieldFill.INSERT)
-    private int delFlag;
 
 }
