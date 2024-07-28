f68b42f5dd5726754760cb184c9a8a5efb398687
lin--jintao <1013240360@qq.com>
2024-07-28 23:40:31 +0800

    refactor: 使用JDK15文本块特性重写sql语句

--- shortlink/project/src/main/java/com/ruoci/shortlink/project/dao/mapper/LinkAccessStatsMapper.java
+++ shortlink/project/src/main/java/com/ruoci/shortlink/project/dao/mapper/LinkAccessStatsMapper.java
@@ -13,9 +13,13 @@ public interface LinkAccessStatsMapper extends BaseMapper<LinkAccessStatsDO> {
     /**
      * 记录基础访问监控数据
      */
-    @Insert("INSERT INTO t_link_access_stats (full_short_url, gid, date, pv, uv, uip, hour, weekday, create_time, update_time, del_flag ) " +
-            "VALUES(#{linkAccessStatsDO.fullShortUrl}, #{linkAccessStatsDO.gid}, #{linkAccessStatsDO.date}, #{linkAccessStatsDO.pv}, #{linkAccessStatsDO.uv}, #{linkAccessStatsDO.uip}, #{linkAccessStatsDO.hour}, #{linkAccessStatsDO.weekday}, NOW(), NOW(), 0 ) " +
-            "ON DUPLICATE KEY UPDATE " +
-            "pv = pv + #{linkAccessStatsDO.pv}, uv = uv + #{linkAccessStatsDO.uv}, uip = uip + #{linkAccessStatsDO.uip};")
+    @Insert(
+            """
+            INSERT INTO t_link_access_stats (full_short_url, gid, date, pv, uv, uip, hour, weekday, create_time, update_time, del_flag )
+            VALUES(#{linkAccessStatsDO.fullShortUrl}, #{linkAccessStatsDO.gid}, #{linkAccessStatsDO.date}, #{linkAccessStatsDO.pv}, #{linkAccessStatsDO.uv}, #{linkAccessStatsDO.uip}, #{linkAccessStatsDO.hour}, #{linkAccessStatsDO.weekday}, NOW(), NOW(), 0)
+            ON DUPLICATE KEY UPDATE
+            pv = pv + #{linkAccessStatsDO.pv}, uv = uv + #{linkAccessStatsDO.uv}, uip = uip + #{linkAccessStatsDO.uip};
+            """
+    )
     void shortLinkStats(@Param("linkAccessStatsDO") LinkAccessStatsDO linkAccessStatsDO);
 }
