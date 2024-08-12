9fd5a2ab579a308aec14c8f6fcded472534dde89
lin--jintao <1013240360@qq.com>
2024-08-12 23:42:15 +0800

    feature: 短链接中台创建短链接接口请求限制

---
+++ shortlink/project/src/main/java/com/ruoci/shortlink/project/config/SentinelRuleConfig.java
@@ -0,0 +1,29 @@
+package com.ruoci.shortlink.project.config;
+
+import com.alibaba.csp.sentinel.slots.block.RuleConstant;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 初始化限流配置
+ * 继承 InitializingBean并重写afterPropertiesSet, 可以在项目启动时就开始限流.
+ */
+@Component
+public class SentinelRuleConfig implements InitializingBean {
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        List<FlowRule> rules = new ArrayList<>();
+        FlowRule createOrderRule = new FlowRule();
+        createOrderRule.setResource("create_short-link");
+        createOrderRule.setGrade(RuleConstant.FLOW_GRADE_QPS);
+        createOrderRule.setCount(1);
+        rules.add(createOrderRule);
+        FlowRuleManager.loadRules(rules);
+    }
+}
--- shortlink/project/src/main/java/com/ruoci/shortlink/project/controller/ShortLinkController.java
+++ shortlink/project/src/main/java/com/ruoci/shortlink/project/controller/ShortLinkController.java
@@ -1,5 +1,6 @@
 package com.ruoci.shortlink.project.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.ruoci.shortlink.project.common.convention.result.Result;
 import com.ruoci.shortlink.project.common.convention.result.Results;
@@ -11,6 +12,7 @@ import com.ruoci.shortlink.project.dto.resp.ShortLinkBatchCreateRespDTO;
 import com.ruoci.shortlink.project.dto.resp.ShortLinkCreateRespDTO;
 import com.ruoci.shortlink.project.dto.resp.ShortLinkGroupCountQueryRespDTO;
 import com.ruoci.shortlink.project.dto.resp.ShortLinkPageRespDTO;
+import com.ruoci.shortlink.project.handler.CustomBlockHandler;
 import com.ruoci.shortlink.project.service.ShortLinkService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -40,6 +42,11 @@ public class ShortLinkController {
      * 创建短链接
      */
     @PostMapping("/api/short-link/v1/create")
+    @SentinelResource(
+            value = "create_short-link",
+            blockHandler = "createShortLinkBlockHandlerMethod",
+            blockHandlerClass = CustomBlockHandler.class
+    )
     public Result<ShortLinkCreateRespDTO> createShortLink(@RequestBody ShortLinkCreateReqDTO requestParam){
         return Results.success(shortLinkService.createShortLink(requestParam));
     }
