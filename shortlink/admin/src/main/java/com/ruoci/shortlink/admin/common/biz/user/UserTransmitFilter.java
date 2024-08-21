64b2bfc28fb63022475ee1d63305d374001f1456
lin--jintao <1013240360@qq.com>
2024-08-21 22:14:29 +0800

    feature: 引入网关改造短链接服务

--- shortlink/admin/src/main/java/com/ruoci/shortlink/admin/common/biz/user/UserTransmitFilter.java
+++ shortlink/admin/src/main/java/com/ruoci/shortlink/admin/common/biz/user/UserTransmitFilter.java
@@ -1,15 +1,13 @@
 package com.ruoci.shortlink.admin.common.biz.user;
 
-import cn.hutool.json.JSONUtil;
-import jakarta.servlet.*;
+import cn.hutool.core.util.StrUtil;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.core.StringRedisTemplate;
-
-import java.io.IOException;
-import java.util.Objects;
-
-import static com.ruoci.shortlink.admin.common.constant.RedisCacheConstant.USER_LOGIN_KEY;
+import lombok.SneakyThrows;
 
 /**
  * 用户信息传输过滤器
@@ -17,20 +15,16 @@ import static com.ruoci.shortlink.admin.common.constant.RedisCacheConstant.USER_
 @RequiredArgsConstructor
 public class UserTransmitFilter implements Filter {
 
-    private final StringRedisTemplate stringRedisTemplate;
-
+    @SneakyThrows
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain){
         HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
-        String requestURI = httpServletRequest.getRequestURI();
-        if (!Objects.equals(requestURI, "/api/short-link/v1/user/login")){
-            String username = httpServletRequest.getHeader("username");
-            String token = httpServletRequest.getHeader("token");
-            Object userInfoJsonStr = stringRedisTemplate.opsForHash().get(USER_LOGIN_KEY + username, token);
-            if (userInfoJsonStr != null){
-                UserInfoDTO userInfoDTO = JSONUtil.toBean(userInfoJsonStr.toString(), UserInfoDTO.class);
-                UserContext.setUser(userInfoDTO);
-            }
+        String username = httpServletRequest.getHeader("username");
+        if (StrUtil.isNotBlank(username)) {
+            String userId = httpServletRequest.getHeader("userId");
+            String realName = httpServletRequest.getHeader("realName");
+            UserInfoDTO userInfoDTO = new UserInfoDTO(userId, username, realName);
+            UserContext.setUser(userInfoDTO);
         }
         try {
             filterChain.doFilter(servletRequest, servletResponse);
